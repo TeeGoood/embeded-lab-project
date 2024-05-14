6e7350fee32bf90559d6b55bfdc5484552177795
boomchanotai <boom9387@hotmail.com>
2024-05-14 17:01:21 +0700

    feat: esp

---
+++ esp/esp.ino
@@ -0,0 +1,12 @@
+#include <SoftwareSerial.h>
+EspSoftwareSerial::UART testSerial;
+void setup() {
+  testSerial.begin(115200, EspSoftwareSerial::SWSERIAL_8N1, D7, D8, false);
+  Serial.begin(9600);
+}
+void loop() {
+  while (testSerial.available() > 0) {
+    String buf = testSerial.readString();
+    Serial.println(buf);
+  }
+}
